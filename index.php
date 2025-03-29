be5661e4e7ddb728a6b414eda6d77bfdb0d860de
Sunginha <contato.vini0dev@gmail.com>
2025-03-29 20:16:33 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Balls</title>
+</head>
+<body>
+  <?php if (true) { // Executing a IF statement using ?php method. for each bracket.?>
+    <p>Hello World!</p>
+  <?php }?>
+</body>
+</html>
